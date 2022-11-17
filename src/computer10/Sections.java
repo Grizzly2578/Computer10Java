d97b22bca01a39b5f7db2bcc85b26d95ea927faa
Grizzly2578 <noah.sitoy001@gmail.com>
2022-11-17 21:10:14 +0800

    Updated! Challenge Accepted Sir Jet!
    I made a Section Selector in just 10 lines!

--- src/computer10/Sections.java
+++ src/computer10/Sections.java
@@ -2,35 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package computer10;
+package computer10;import java.io.FileNotFoundException;import java.io.FileReader;import java.io.IOException;import java.util.Scanner;import org.json.simple.JSONObject;import org.json.simple.parser.JSONParser;import org.json.simple.parser.ParseException;
 
-import java.util.Scanner;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 /**
  *
  * @author Noah Sitoy
  */
-public class Sections {
-    public static void main(String[] args) {
-        JSONObject my_dict = new JSONObject();
-        my_dict.put("1", "Topaz");
-        my_dict.put("2", "Sapphire");
-        my_dict.put("3", "Sardonyx");
-        my_dict.put("4", "Jascint");
-        my_dict.put("5", "Emerald");
-        my_dict.put("6", "Jasper");
-        my_dict.put("7", "Amethyst");
-        my_dict.put("8", "Jade");
-        while (true){
-            Scanner _input1 = new Scanner(System.in);
-            System.out.println("Enter a number!");
-            String num1 = _input1.nextLine();
-//            String = my_dict.get(num1);
-             
-            System.out.println(num1);
-            break;
-            
-        }
-    }
-}
+public class Sections {public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {JSONObject my_dict= (JSONObject) new JSONParser().parse(new FileReader("sections.json"));while (true){Scanner _input1 = new Scanner(System.in);System.out.println("Enter a number!");String output = _input1.nextLine();var value = my_dict.get(output);if (null == (String) value){System.out.println("Section not found!");}else {System.out.println((String) value);}continue;}}}
