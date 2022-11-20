96b8a486982be1ca31afee3b2e99de817f5f7bc0
Grizzly2578 <noah.sitoy001@gmail.com>
2022-11-20 13:22:25 +0800

    converted most files to one lines!

--- src/computer10/NameInput.java
+++ src/computer10/NameInput.java
@@ -1,14 +1,1 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package computer10;
-
-import java.util.Scanner;
-public class NameInput {
-    public static void main(String[]args){
-        Scanner _input1=new Scanner(System.in);
-        System.out.println("Enter Username:");
-        System.out.println("Hello "+_input1.nextLine()+"!");
-    }
-}
+package computer10;import java.util.Scanner;public class NameInput{public static void main(String[]args){Scanner _input1=new Scanner(System.in);System.out.println("Enter Username:");System.out.println("Hello "+_input1.nextLine()+"!");}}//Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
