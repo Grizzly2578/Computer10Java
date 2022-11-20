ab45a9b80f7bd18cc3c7121f1eb58b126d7db3dc
Grizzly2578 <noah.sitoy001@gmail.com>
2022-11-20 16:03:11 +0800

    .

--- src/computer10/IF_Sections.java
+++ src/computer10/IF_Sections.java
@@ -12,33 +12,21 @@ import java.util.Scanner;
  */
 public class IF_Sections {
     public static void main (String[] args) {
-        while (true) {
+        while(true){
             Scanner _input1=new Scanner(System.in);
             System.out.println("Enter a number");
             String num1=_input1.nextLine();
-            if(num1.equals("1")){
-                System.out.println("7 - Topaz");
-            }
-            if(num1.equals("2")){
-                System.out.println("7 - Sapphire");
-            }
-            if(num1.equals("3")){
-                System.out.println("8 - Sardonyx");
-            }
-            if(num1.equals("4")){
-                System.out.println("8 - Jascint");
-            }
-            if(num1.equals("5")){
-                System.out.println("9 - Jasper");
-            }
-            if(num1.equals("6")){
-                System.out.println("9 - Emerald");
-            }
-            if(num1.equals("7")){
-                System.out.println("10 - Jade");
-            }
-            if(num1.equals("8")){
-                System.out.println("10 - Amethyst");
+            switch(num1){
+                case"1"->System.out.println("7 - Topaz");
+                case"2"->System.out.println("7 - Sapphire");
+                case"3"->System.out.println("8 - Sardonyx");
+                case"4"->System.out.println("8 - Jascint");
+                case"5"-> System.out.println("9 - Jasper");
+                case"6"-> System.out.println("9 - Emerald");
+                case"7"-> System.out.println("10 - Jade");
+                case"8"-> System.out.println("10 - Amethyst");
+                default->{
+                }
             }
         }
     }
