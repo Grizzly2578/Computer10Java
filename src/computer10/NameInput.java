20e31ba99aeeb77d482eed7ef8cb1e4a10a59301
Grizzly2578 <noah.sitoy001@gmail.com>
2022-11-20 11:18:28 +0800

    made some changes to compress file size

--- src/computer10/NameInput.java
+++ src/computer10/NameInput.java
@@ -5,23 +5,10 @@
 package computer10;
 
 import java.util.Scanner;
-
-/**
- *
- * @author Noah Sitoy
- */
 public class NameInput {
-    public static void main(String[] args) {
-        Scanner _input1 = new Scanner(System.in);
+    public static void main(String[]args){
+        Scanner _input1=new Scanner(System.in);
         System.out.println("Enter Username:");
-        
-        String userName = _input1.nextLine();
-        
-        if ("noah".equals(userName.toLowerCase())){
-            System.out.println("Hello Noah! hahahha you are the creator of this Code!");
-        }
-        else if (!"Noah".equals(userName.toLowerCase())){
-            System.out.println("Hello " + userName + " You are not Noah!");
-        }
+        System.out.println("Hello "+_input1.nextLine()+"!");
     }
 }
