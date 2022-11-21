b6ab9759f4256bde92aee1ee2fcbb960f4faacc5
Grizzly2578 <noah.sitoy001@gmail.com>
2022-11-21 14:31:41 +0800

    added try with resources

--- src/computer10/Integer_Operations.java
+++ src/computer10/Integer_Operations.java
@@ -2,17 +2,22 @@ package computer10;
 import java.util.Scanner;
 public class Integer_Operations {
     public static void main(String[] args) {
-        var _input1 = new Scanner(System.in);
-        System.out.println("Your first Number:");
-        double num1 = _input1.nextDouble(); // Variable for the first number!
-        Scanner _input2 = new Scanner(System.in);
-        System.out.println("Your second Number:");
-        double num2 = _input2.nextDouble(); // Variable for the second number!
-        double sum = num1 + num2; // Variable for the sum of the numbers!
-        double difference = num1 - num2; // Variable for the difference of the numbers!
-        double quotient = num1 / num2; // Variable for the quotient of the numbers!
-        double product = num1 * num2; // Variable for the product of the numbers!
-        _input1.close(); // Closed the inputstream to avoid resource leaks!
+        Scanner _input2;
+        double sum;
+        double difference;
+        double quotient;
+        double product;
+        try(var _input1=new Scanner(System.in)){
+            System.out.println("Your first Number:");
+            double num1 =_input1.nextDouble(); // Variable for the first number!
+            _input2 =new Scanner(System.in);
+            System.out.println("Your second Number:");
+            double num2 =_input2.nextDouble(); // Variable for the second number!
+            sum =num1+num2; // Variable for the sum of the numbers!
+            difference =num1 - num2; // Variable for the difference of the numbers!
+            quotient =num1/num2; // Variable for the quotient of the numbers!
+            product =num1*num2; // Variable for the product of the numbers!
+        }
         _input2.close(); // Closed the inputstream to avoid resource leaks!
         System.out.println("sum:"+ sum); // Display the output output of the sum
         System.out.println("difference:"+ difference); // Display the output output of the differnce!
