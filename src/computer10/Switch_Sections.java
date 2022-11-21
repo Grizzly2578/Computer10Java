90f7512ede4889fae78fd5f8f8be16f026a28dc9
Grizzly2578 <noah.sitoy001@gmail.com>
2022-11-21 12:58:43 +0800

    added some sections

--- src/computer10/Switch_Sections.java
+++ src/computer10/Switch_Sections.java
@@ -17,14 +17,18 @@ public class Switch_Sections{
             System.out.println("Enter a number");
             String num1=_input1.nextLine();
             switch(num1){
-                case"1"->System.out.println("7 - Topaz");
+                case"1"->System.out.println("Topaz");
                 case"2"->System.out.println("7 - Sapphire");
-                case"3"->System.out.println("8 - Sardonyx");
+                case"3"->System.out.println("Sardonyx");
                 case"4"->System.out.println("8 - Jascint");
-                case"5"-> System.out.println("9 - Jasper");
-                case"6"-> System.out.println("9 - Emerald");
-                case"7"-> System.out.println("10 - Jade");
-                case"8"-> System.out.println("10 - Amethyst");
+                case"5"->System.out.println("Jasper");
+                case"6"->System.out.println("9 - Emerald");
+                case"7"->System.out.println("Jade");
+                case"8"->System.out.println("7 - Amethyst");
+                case"9"->System.out.println("Beryl");
+                case"10"->System.out.println("10 - Crysolite");
+                case"11"->System.out.println("Ruby");
+                case"12"->System.out.println("12 - Chalcedony");
                 default->{
                 }
             }
