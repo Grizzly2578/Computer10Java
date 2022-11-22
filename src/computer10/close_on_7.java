0fa835eefa91dbe521651aaa7dc37b2695f489ef
Grizzly2578 <noah.sitoy001@gmail.com>
2022-11-22 16:28:15 +0800

    Update close_on_7.java

--- src/computer10/close_on_7.java
+++ src/computer10/close_on_7.java
@@ -1,1 +1,37 @@
-package computer10;import java.util.Scanner;public class close_on_7{public static void main(String[]args){while(true){var input1=new Scanner(System.in);int num = input1.nextInt();System.out.println("The program is still running!");switch(num){case 7 -> System.exit(0);default -> System.out.println("Program Is still running");}}}}
+package computer10;
+
+import java.util.Scanner;
+
+public class close_on_7 {
+
+    public static void main(String[] args) {
+        while (true) {
+            Scanner _input1 = new Scanner(System.in);
+            System.out.println("Enter a number");
+            int num1 = _input1.nextInt();
+            switch (num1) {
+                case 1 ->
+                    System.out.println("Topaz");
+                case 2 ->
+                    System.out.println("7 - Sapphire");
+                case 3 ->
+                    System.out.println("Sardonyx");
+                case 4 ->
+                    System.out.println("8 - Jascint");
+                case 5 ->
+                    System.out.println("Jasper");
+                case 6 ->
+                    System.out.println("9 - Emerald");
+                case 7 ->
+                    System.out.println("Jade");
+                case 8 ->
+                    System.out.println("10 - Amethyst");
+                case 9 ->
+                    System.out.println("Beryl");
+                case 10 ->
+                    System.out.println("11 - Crysolite");
+                case 11 ->
+                    System.out.println("Ruby");
+                case 12 ->
+                    System.out.println("12 - Chalcedony");
+                case  17->System.exit(0);default->System.out.println("Unknown Section");}}}}
